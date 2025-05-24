64a63f32d455848f44a5210cc172fe232c831b02
josefranval <josefranval88@gmail.com>
2025-05-24 15:14:28 -0300

    config: config initial

---
+++ src/test/java/com/buhoristeca/lector/LectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buhoristeca.lector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
